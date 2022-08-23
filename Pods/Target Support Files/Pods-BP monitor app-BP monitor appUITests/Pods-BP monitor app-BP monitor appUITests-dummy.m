a32a40d33bcc71f4b84019227e93ce817e7e31db
chaitraBL <chaitra11397@gmail.com>
2022-08-23 17:14:45 +0530

    BLE changes

---
+++ Pods/Target Support Files/Pods-BP monitor app-BP monitor appUITests/Pods-BP monitor app-BP monitor appUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BP_monitor_app_BP_monitor_appUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BP_monitor_app_BP_monitor_appUITests
+@end
---
+++ Pods/Target Support Files/Pods-BP monitor appTests/Pods-BP monitor appTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BP_monitor_appTests : NSObject
+@end
+@implementation PodsDummy_Pods_BP_monitor_appTests
+@end
