7ea72384608c94a36310fcbf329682dcef4a6390
Chaitra <chaitra11397@gmail.com>
2022-09-14 13:38:48 +0530

    BLE changes

--- app/src/main/java/com/example/bpmonitorbleintegration/home/HomePage.java
+++ app/src/main/java/com/example/bpmonitorbleintegration/home/HomePage.java
@@ -612,15 +612,15 @@ public class HomePage extends AppCompatActivity implements BottomNavigationView.
             progressBar2.setProgress(diastolic);
             progressBar2.setProgressTintList(ColorStateList.valueOf(Color.parseColor("#008000")));
         }
-        else if (diastolic <= 89) {
+        else if (diastolic > 80 && diastolic < 89) {
             progressBar2.setProgress(diastolic);
             progressBar2.setProgressTintList(ColorStateList.valueOf(Color.parseColor("#FFD700")));
         }
-        else if (diastolic <= 99) {
+        else if (diastolic > 89 && diastolic < 99) {
             progressBar2.setProgress(diastolic);
             progressBar2.setProgressTintList(ColorStateList.valueOf(Color.parseColor("#FFA500")));
         }
-        else if (diastolic <= 109) {
+        else if (diastolic > 99 && diastolic < 109) {
             progressBar2.setProgress(diastolic);
             progressBar2.setProgressTintList(ColorStateList.valueOf(Color.parseColor("#FF8C00")));
         }
